89b768997be3ed6e25658474bbb762c45cf36d4d
오가희 <72989911+ogh010@users.noreply.github.com>
2025-05-25 21:38:04 +0900

    [level 0] Title: 접미사 배열, Time: 0.48 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,15 +1,15 @@
 import java.util.Arrays;
 class Solution {
     public String[] solution(String my_string) {
-        int size = my_string.length();
+        int size = my_string.length(); // 문자열 길이만큼 공간 확보
         String[] answer = new String[size];
         
         for(int i=0; i<my_string.length(); i++){
-            
+            // i 부터 끝까지 자른 문자열 answer[i] 에 저장 
             answer[i] = my_string.substring(i);
         }
         
-        Arrays.sort(answer);
+        Arrays.sort(answer); // 사전순(오름차순)
         
         return answer;
     }
